81264bf0d8762312d31768e89218bbda4ae01803
Praveenspkj <praveenspkofficial@gmail.com>
2025-07-08 07:58:23 +0530

    Muruga 8-6-2025 recursion basics

---
+++ src/Recursion/Questions/medium/CheckStringPalindrome.java
@@ -0,0 +1,19 @@
+package Recursion.Questions.medium;
+
+public class CheckStringPalindrome {
+    public static void main(String[] args) {
+        String str="abcfba";
+        boolean ans=checkP(str,0,str.length()-1);
+        System.out.println(ans);
+    }
+
+    private static boolean checkP(String str,int s,int e) {
+        if(s>=e){
+            return true;
+        }
+        if(str.charAt(s)!=str.charAt(e)){
+            return false;
+        }
+        return checkP(str,s+1,e-1);
+    }
+}
